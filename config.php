b0db01604ffc8da9904cd1a2628d8c826212fe5a
safriyan1608 <61663248+safriyan1608@users.noreply.github.com>
2020-03-02 01:28:26 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
 
-$email = "witotoa@gmail.com";
-$address = "1B2br9TmDE55rSDKypifZjDih7gcBM2PV3";
+$email = "safriyan1608@gmail.com";
+$address = "35ntjNUncgnYJXabLzahx7QKaX49Scvc9G";
 
 /* Link Pass :    https://semawur.com/0dGrBL7wm0   */
 
 $pass_scrip = "hhhskej";
 
 
-//https://www.hachwaclaims.xyz/?r=15856
 
+https://www.hachwaclaims.xyz/?r=17975
+© 2020 GitHub, Inc
